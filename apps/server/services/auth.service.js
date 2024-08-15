0333d48f7a174355fd994c32073e33e24a18877c
Parle648 <sashahoumi@gmail.com>
2024-08-15 10:50:06 +0300

    + Create auth endpoints

--- apps/server/services/auth.service.js
+++ apps/server/services/auth.service.js
@@ -1,14 +1,17 @@
 import jwt from "jsonwebtoken";
 import User from "../data/models/user.model.js";
 import { SECRET } from "../config/jwt.js";
+import bcrypt from "bcrypt";
 
 const authService = {
   signUp: async (req, res) => {
     try {
       const userDTO = req.body;
-      const createdUser = await User.create(userDTO);
-
-      console.log(createdUser);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const createdUser = await User.create({
+        ...userDTO,
+        password: hashedPassword,
+      });
 
       if (createdUser.error === undefined) {
         const token = jwt.sign({ createdUser }, SECRET);
@@ -24,7 +27,14 @@ const authService = {
   },
   login: async (req, res) => {
     try {
-      return res.status(200).send({ message: "user logged in successfully" });
+      const isUserExist = await User.findOne(req.body);
+
+      if (isUserExist.error === undefined) {
+        const token = jwt.sign({ isUserExist }, SECRET);
+        return res
+          .status(200)
+          .send({ message: "user logged in successfully", token });
+      }
     } catch (error) {
       return res.status(500).send({ error: error });
     }
